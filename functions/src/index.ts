7c1b562b015a3687ebb7b72a54325e958bd48305
tfxsoftware <aswespeak.tfx@gmai.com>
2023-09-18 13:33:14 -0300

    create and find restaurant

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,67 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const db = admin.firestore();
+const auth = admin.auth();
+const app = express();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(bodyParser.json());
+
+
+app.post('/addNewRestaurante', async (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+
+    const batch = db.batch();
+    const docRef = db.collection('Restaurantes').doc();
+
+    const { uid } = await auth.createUser({
+      uid: docRef.id,
+      email: data.email,
+      password: data.senha,
+      displayName: data.nome
+    });
+
+    batch.set(docRef, {
+      ...data,
+      uid
+    });
+    await batch.commit();
+
+    res.status(200).json({ "message": `Restaurante and Auth User created with ID: ${uid}` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Falha ao registrar restaurante' });
+  }
+});
+
+
+app.get('/getRestaurantById', async (req: express.Request, res: express.Response) => {
+  try {
+    
+    const restaurantId = req.query.id as string;
+
+    
+    const restaurantDoc = await db.collection('Restaurantes').doc(restaurantId).get();
+
+    
+    if (!restaurantDoc.exists) {
+      return res.status(404).json({ error: 'Restaurant not found' });
+    }
+
+    
+    const restaurantData = restaurantDoc.data();
+
+    
+    return res.status(200).json(restaurantData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'An error occurred while fetching the restaurant' });
+  }
+});
+
+exports.api = functions.region('southamerica-east1').https.onRequest(app);
