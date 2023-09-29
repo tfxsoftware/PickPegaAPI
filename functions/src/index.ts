d5d5187d6f3cf2de5c67ccd4825bcb4a8a3813a5
tfxsoftware <aswespeak.tfx@gmai.com>
2023-09-29 11:47:26 -0300

    CRUD implementado

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 admin.initializeApp();
 
@@ -10,7 +11,10 @@ const auth = admin.auth();
 const app = express();
 
 app.use(bodyParser.json());
-// https://southamerica-east1-pick-pega.cloudfunctions.net/api >> HTTP PARA FAZER REQUISIÇÕES (SEGUIDO DA FUNÇÃO QUE QUER CHAMAR EX: /addNewRestaurante)
+app.use(cors())
+//https://southamerica-east1-pick-pega.cloudfunctions.net/api >> HTTP PARA FAZER REQUISIÇÕES (SEGUIDO DA FUNÇÃO QUE QUER CHAMAR EX: /addNewRestaurante)
+
+
 //FUNÇÃO QUE ADICIONA NOVO RESTAURANTE E USUARIO AUTH PARA LOGIN:
 app.post('/addNewRestaurante', async (req: express.Request, res: express.Response) => {
   try {
@@ -31,7 +35,7 @@ app.post('/addNewRestaurante', async (req: express.Request, res: express.Respons
     });
     await batch.commit(); // aqui executamos as 2 ações: criamos o auth user e o restaurante no banco de dados
 
-    res.status(200).json({ "message": `Restaurante and Auth User created with ID: ${uid}` }); //log
+    res.status(200).json({ "message": `Restaurante e authuser criado com o id: ${uid}` }); //log
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Falha ao registrar restaurante' });//log
@@ -49,7 +53,7 @@ app.get('/getRestaurantById', async (req: express.Request, res: express.Response
 
     
     if (!restaurantDoc.exists) {
-      return res.status(404).json({ error: 'Restaurant not found' }); // se não existir retornaremos um erro!
+      return res.status(404).json({ error: 'Restaurante não encontrado' }); // se não existir retornaremos um erro!
     }
 
     
@@ -59,8 +63,43 @@ app.get('/getRestaurantById', async (req: express.Request, res: express.Response
     return res.status(200).json(restaurantData); // aqui retornamos o dados da requisição!
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'An error occurred while fetching the restaurant' });
+    return res.status(500).json({ error: 'Um erro ocorreu!' });
   }
 });
+//FUNÇÃO QUE DELETA RESTAURANTES
+app.delete('/deleteRestaurante/:id', async (req: express.Request, res: express.Response) => {
+  try {
+    const restaurantId = req.params.id; // Puxa o id do restaurante por parametros de rota
+
+    // Deleta o documento do firestore
+    await db.collection('Restaurantes').doc(restaurantId).delete();
+
+    //deleta o authuser (conta de autenticação)
+    await auth.deleteUser(restaurantId);
+
+    res.status(200).json({ message: 'Restaurante excluído com sucesso' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Falha ao excluir restaurante' });
+  }
+});
+//FUNÇÃO QUE EDITA O RESTAURANTE
+app.put('/editRestaurant/:id', async (req: express.Request, res: express.Response) => {
+  try {
+    const restaurantId = req.params.id; // pega o id do restaurante
+    const updatedData = req.body; // aqui é o novo restaurante a ser trocado (LEMBRAR DE ENVIAR UM OBJETO QUE PREENCHE TODOS OS PARAMETROS MESMO QUE ELES NAO SERAO EDITADOS)
+
+    // faz o update no firebase
+    await db.collection('Restaurantes').doc(restaurantId).update(updatedData);
+
+    res.status(200).json({ message: 'Restaurante atualizado com sucesso' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Falha ao atualizar restaurante' });
+  }
+});
+
+
+
 
 exports.api = functions.region('southamerica-east1').https.onRequest(app);
